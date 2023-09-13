8b7b9997f337a852817fff6203c9efca0f83b9a7
PintooMali <pintoomali1@gmail.com>
2023-09-13 16:05:36 +0530

    api url updated

--- src/components/pokemon.js
+++ src/components/pokemon.js
@@ -2,7 +2,7 @@ import React from "react";
 const Card=({name,type,id})=>{
     return(
         <div className="tc bg-light-green br3 pa3 ma3 dib grow shadow-5">
-            <img alt="pokemons" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} widht="300" height="300"></img>
+            <img alt="pokemons" src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${id}.svg`} widht="300" height="300"></img>
             <div>
             <h2>{name}</h2>
             <p>Type: {type}</p>
